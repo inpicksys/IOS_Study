25a7a5fb95ac1b4fb0da3d9dfa04f6105408d087
InPickSys <strmisha.s@gmail.com>
2020-04-25 23:01:03 +0300

    Initial commit

---
+++ MyFirstApp/MyFirstApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by Misha Strutinsky on 25.04.2020.
+//  Copyright © 2020 InPickSys. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
