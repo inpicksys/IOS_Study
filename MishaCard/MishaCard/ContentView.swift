6a1eb12ac5d7caab72f65517a1ca6e78336c9eac
InPickSys <strmisha.s@gmail.com>
2020-12-26 19:40:38 +0200

    -swift ui & persistance storage setup

--- MishaCard/MishaCard/ContentView.swift
+++ MishaCard/MishaCard/ContentView.swift
@@ -7,13 +7,34 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
     var body: some View {
         ZStack {
-            Color(.green)
+            Color(red: 0.09, green: 0.63, blue: 0.52, opacity: 0.90)
                 .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-            Text("Hello, world!")
-                .padding()
+            VStack {
+                Image("MishaStrutinsky")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width:150.0, height:150)
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    .overlay(Circle().stroke(Color.white, lineWidth: 5)
+                )
+                Text("Misha Strutinsky")
+                    .font(Font.custom("Pacifico-Regular", size: 40))
+                    .bold()
+                    .foregroundColor(.white)
+                    .padding()
+                Text("Fullstack Developer")
+                    .foregroundColor(.white)
+                    .font(.system(size: 25))
+                Divider()
+                InfoView(text: "+380 66 84 84 064", imageName: "phone.fill")
+                InfoView(text: "strmisha.s@gmail.com", imageName: "envelope.fill")
+                
+                    
+            }
         }
         
     }
@@ -24,3 +45,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
---
+++ MishaCard/MishaCard/InfoView.swift
@@ -0,0 +1,33 @@
+//
+//  InfoView.swift
+//  MishaCard
+//
+//  Created by Misha Strutinsky on 24.12.2020.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    
+    let text: String
+    let imageName: String
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25)
+            .fill(Color.white)
+            .frame(height: 50)
+            .overlay(HStack {
+                Image(systemName: imageName)
+                    .foregroundColor(.green)
+                Text(text)
+            })
+            .padding(.all)
+    }
+}
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView(text: "Hello", imageName: "phone")
+            .previewLayout(.sizeThatFits)
+    }
+}
