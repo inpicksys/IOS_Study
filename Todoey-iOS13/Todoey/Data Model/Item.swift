7ef719eb833a634e3cb8bf70ec449f8c4900637d
InPickSys <inpicksys@gmail.com>
2021-03-07 02:03:54 +0200

    update todo app

---
+++ Todoey-iOS13/Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Misha Strutinsky on 06.03.2021.
+//  Copyright © 2021 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done: Bool = false
+}
